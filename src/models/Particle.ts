009856a9bd7b3d4b29cbd6d8a30f7695cf800061
Icaro <luizicaro.s@gmail.com>
2023-11-25 11:44:20 -0300

    Add checkPerformance()

--- src/models/Particle.ts
+++ src/models/Particle.ts
@@ -2,27 +2,54 @@ import OptType from '../enums/optimizationType';
 import ObjectiveFn from '../interfaces/objectiveFunction';
 
 export default class Particle {
-    private static _objectiveFunction: ObjectiveFn;
-    private static _optimizationType: OptType;
-    private static _minPosition: number;
-    private static _maxPosition: number;  
-    private static _globalBest: number[];
-
     private _velocity: number[];
     private _position: number[];
     private _personalBest: number[];
+    private static _globalBest: number[];
+
+    private static _objectiveFunction: ObjectiveFn;
+    private static _optimizationType: OptType;
+    private static _minPosition: number[];
+    private static _maxPosition: number[];
 
     constructor(dimensions: number) {
+        if (!Particle._objectiveFunction || !Particle._optimizationType || Particle._minPosition.length === 0 || Particle._maxPosition.length === 0)
+            throw new Error('Set static swarm Params!');
+
+        if (Particle._minPosition.length !== dimensions || Particle._maxPosition.length !== dimensions)
+            throw new Error('minPosition and maxPosition must have the same length as dimensions!');
+
         this._velocity = new Array(dimensions).fill(0);
-        this._position = new Array(dimensions).fill(0).map(() => {
-            return (Math.random() * (Particle._maxPosition - Particle._minPosition)) + Particle._minPosition;
+        this._position = new Array(dimensions).fill(0).map((_, index) => {
+            return (Math.random() * (Particle._maxPosition[index] - Particle._minPosition[index])) + Particle._minPosition[index];
         });
 
         this._personalBest = this._position;
         if (!Particle._globalBest) Particle._globalBest = this._position;
-    }    
+        else this.checkPerformance();
+    }
 
-    public static setSwarmParams(objectiveFunction: ObjectiveFn, optimizationType: OptType, minPosition: number, maxPosition: number) {
+    private checkPerformance(): void {
+        const performance = Particle._objectiveFunction.objectiveFunction(...this._position);
+        const globalBestValue = Particle._objectiveFunction.objectiveFunction(...Particle._globalBest);
+        const personalBestValue = Particle._objectiveFunction.objectiveFunction(...this._personalBest);
+        if ((Particle._optimizationType === 'max' && performance > globalBestValue)
+            || (Particle._optimizationType === 'min' && performance < globalBestValue)) {
+            Particle._globalBest = this._position;
+            this._personalBest = this._position;
+        } else if ((Particle._optimizationType === 'max' && performance > personalBestValue)
+            || (Particle._optimizationType === 'min' && performance < personalBestValue)) {
+            this._personalBest = this._position;
+        } else {
+            throw new Error('checkPerformance Error!'); //Just a test, Remove Later
+        }
+    }
+
+    public get globalBest() {
+        return Particle._globalBest;
+    }
+
+    public static setSwarmParams(objectiveFunction: ObjectiveFn, optimizationType: OptType, minPosition: number[], maxPosition: number[]): void {
         Particle._objectiveFunction = objectiveFunction;
         Particle._optimizationType = optimizationType;
         Particle._minPosition = minPosition;
