fbf57933dcaf2a4da9b8cc104da46381a6edbbab
Icaro <luizicaro.s@gmail.com>
2023-11-27 21:31:20 -0300

    Import objectiveFunction

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
 import PSO from './models/PSO';
 import OptType from './enums/optimizationType';
 import * as fs from 'fs';
-
-function objectiveFn(x: number): number {
-    // return Math.pow(x, 3) - (1.5 * x * y);
-    // return x * Math.sin(10 * Math.PI * x) + 1;
-    return Math.pow(x, 2) - 3 * x + 4;
-}
+import objectiveFn from './objectives/objectiveFunction';
 
 const objFn = { objectiveFunction : objectiveFn };
 const optimizationType = OptType.MIN;
-const minPosition = [-10];
-const maxPosition = [10];
+const minPosition = [-500, -500];
+const maxPosition = [500, 500];
 const decimalPrecision = 5;
 
-const swarmSize = 4;
+const swarmSize = 35;
 const dimensions = minPosition.length;
-const generation = 25;
+const generation = 100;
+
+// console.log(objectiveFn(-420, 500));
+// console.log(objectiveFn(-65, -230));
+// console.log(objectiveFn(-430, 110));
 
 PSO.setSwarmParams(objFn, optimizationType, minPosition, maxPosition);
 try {
